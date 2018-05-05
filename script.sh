f3646950a793b2b2de58f09f1b070cab18df045a
quiye <zamasu@gmail.com>
2018-05-05 20:59:03 +0900

    2018年  5月  5日 土曜日 20:59:03 JST

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 cd /home/pi/fitbitt/
 cat `ls -l 20* |grep -v 'pi.*pi.* 0 .*'|grep -v 'pi.*pi.* 382 .*'|tail -n 1| awk '{print $9}'` |sed "s/'/\"/g"|jq '(."activities-heart-intraday"[]|reverse)[0:4]'| grep value |awk '{X += $2}END{printf("%.1f %.3f\n",X / NR, 60 / ( X / NR) )}'> /home/pi/hr/hr.txt
 cd /home/pi/hr/
-sed "s/^at .*/at `date`/g" -i index.html
-sed "s/心拍数.*分/心拍数: `cat hr.txt|awk '{print $1}'` 回\/分/g" -i index.html
+#sed "s/^at .*/at `date`/g" -i index.html
+#sed "s/心拍数.*分/心拍数: `cat hr.txt|awk '{print $1}'` 回\/分/g" -i index.html
 sed "s/-webkit-animation: animationTest .*s/-webkit-animation: animationTest `cat hr.txt|awk '{print $2}'`s/g" -i index.css
 sed "s/index\.css?v=[0-9]*/index\.css?v=`date +%s`/g" -i index.html
 echo "{\"heartrate\": \"`cat hr.txt|awk '{print $1}'`\", \"date\": \"`date`\"}" > hr.json
